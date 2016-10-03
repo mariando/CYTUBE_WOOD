3223de48bbd1e822b7db37faefe584889701ad42
mariando <mateusz010.mk@gmail.com>
2016-10-03 19:56:31 +0200

    Update 1111111.js

--- master/1111111.js
+++ master/1111111.js
@@ -14,7 +14,7 @@ $("#rightpane").after(
 '<a href="https://www.youtube.com/user/Owsiaknet"><img src="//i.imgur.com/BAtXGNU.jpg" class="kek" /></a>' +
 '<a href="https://www.facebook.com/kreciola"><img src="//i.imgur.com/YObDgQC.png" class="kek" /></a>' +
 '<a href="https://twitter.com/woodstockpoland?lang=pl"><img src="//i.imgur.com/YOS52BW.png" class="kek" /></a>' +
-'<a href="https://cytu.be/r/X-Japan"><img src="//i.imgur.com/L2SxqcD.png" class="kek" /></a>' +
+'<a href="https://www.instagram.com/woodstockpoland/?hl=pl"><img src="//i.imgur.com/L2SxqcD.png" class="kek" /></a>' +
 '<a href="http://taima.tv/r/BABYMETAL"><img src="//i.imgur.com/pS0lJ9W.png" class="kek" /></a>' +
 '</div></div></div>');
 // The HorizontalScroller Class accepts a jQuery object as its only argument
